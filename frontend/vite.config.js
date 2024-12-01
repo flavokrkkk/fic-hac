97e800d04c2e85039db31724a0f2d75f38f2ef3d
flavokrkkk <145288995+flavokrkkk@users.noreply.github.com>
2024-12-01 05:52:13 +0300

    finish build

---
+++ frontend/vite.config.js
@@ -0,0 +1,37 @@
+import react from "@vitejs/plugin-react"
+import path from "path"
+import { defineConfig } from "vite"
+import cesium from "vite-plugin-cesium"
+export default defineConfig({
+  server: {
+    host: true,
+    port: 5173
+  },
+  esbuild: {
+    target: "esnext",
+    platform: "node"
+  },
+  build: {
+    outDir: "dist"
+  },
+  plugins: [react(), cesium()],
+  resolve: {
+    alias: {
+      "@app": path.resolve(__dirname, "./src/app"),
+      "@pages": path.resolve(__dirname, "./src/pages"),
+      "@widgets": path.resolve(__dirname, "./src/widgets"),
+      "@features": path.resolve(__dirname, "./src/features"),
+      "@entities": path.resolve(__dirname, "./src/entities"),
+      "@shared": path.resolve(__dirname, "./src/shared"),
+      "@lib": path.resolve(__dirname, "./src/lib")
+    }
+  },
+
+  build: {
+    rollupOptions: {
+      output: {
+        chunkFileNames: "[name]-[hash].js"
+      }
+    }
+  }
+})
